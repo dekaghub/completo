e755be18c5c2ac7d074f6e9d052c1fa4f1172943
Susanta <sdeka@bgsu.edu>
2020-09-10 18:17:48 -0400

    prefix app css commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import FixedBar from './components/FixedBar.jsx'
+import FixedBar from './components/FixedBar.jsx';
+import Shuffle from './components/Shuffle';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>COMPLETO</h1>
-      <FixedBar />
+      <div className="grid-container">
+        <div className="navbar">
+          <h1>COMPLETO</h1>
+        </div>
+        <div className="content">
+          <FixedBar className="fixed-bar" />          
+          <FixedBar className="fixed-bar" />          
+          <FixedBar className="fixed-bar" />          
+          <FixedBar className="fixed-bar" />          
+          <FixedBar className="fixed-bar" />          
+        </div>
+        <div className="bottom">
+          <Shuffle />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Shuffle.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Shuffle extends Component {
+    render() {
+        return (
+            <div>
+                Shuffle buttons
+            </div>
+        )
+    }
+}
+
+export default Shuffle;
